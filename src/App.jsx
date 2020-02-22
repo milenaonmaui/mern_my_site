d4002ac8809068f4dcf85082c5becaf87e2c9eba
Milena Kari <milenakari@yahoo.com>
2020-02-21 16:58:10 -1000

    Add basic React class

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-const continents = ['Africa', 'America', 'Australia', 'New Zealand', 'Europe', 'Asia']
-const helloContinents = Array.from(continents, c => `Hello, ${c}!`)
-const message = helloContinents.join(' ')
-const element = (  
-  <div title="Outer div">    
-    <h1>{message}</h1>  
-  </div>);
+class HelloWorld extends React.Component {
+  render(){
+    const continents = ['Africa', 'America', 'Australia', 'New Zealand', 'Europe', 'Asia']
+    const helloContinents = Array.from(continents, c => `Hello, ${c}!`)
+    const message = helloContinents.join(' ');
+    return(
+      <div title="Outer div"> 
+        <h2>Using React</h2>   
+        <h1>{message}</h1>  
+      </div>
+    )
+  }
+}
+
+
+const element = <HelloWorld/> 
+
 ReactDOM.render(element, document.getElementById('contents'));
